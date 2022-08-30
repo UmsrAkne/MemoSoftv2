0611484be16f272abfdd61e0651e00dd7490fef7
Akn <sibata486@gmail.com>
2022-08-30 19:55:38 +0900

    add / CommentDbContext.cs

---
+++ MemoSoftv2/Models/DBs/CommentDbContext.cs
@@ -0,0 +1,23 @@
+﻿namespace MemoSoftv2.Models.DBs
+{
+    using Microsoft.EntityFrameworkCore;
+    using Npgsql;
+
+    public class CommentDbContext : DbContext
+    {
+        public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            NpgsqlConnectionStringBuilder builder = new NpgsqlConnectionStringBuilder();
+
+            builder.Port = 5433;
+            builder.Username = "postgres";
+            builder.Password = "password";
+            builder.Host = "localhost";
+            builder.Database = "testdb";
+
+            optionsBuilder.UseNpgsql(builder.ToString());
+        }
+    }
+}
