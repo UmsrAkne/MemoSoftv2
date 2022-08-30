d370dbc85a30c2416fbdc702c6fd7719470c4187
Akn <sibata486@gmail.com>
2022-08-31 01:07:06 +0900

    add / データベースへの接続に失敗した場合の処理を追加

--- MemoSoftv2/ViewModels/MainWindowViewModel.cs
+++ MemoSoftv2/ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,27 @@
 ﻿namespace MemoSoftv2.ViewModels
 {
+    using MemoSoftv2.Models.DBs;
     using Prism.Mvvm;
 
     public class MainWindowViewModel : BindableBase
     {
         private string title = "Prism Application";
         private string inputText;
+        private string systemMessage;
+
+        private CommentDbContext commentDbContext = new CommentDbContext();
 
         public MainWindowViewModel()
         {
+            try
+            {
+                commentDbContext.Database.EnsureCreated();
+            }
+            catch (Npgsql.NpgsqlException)
+            {
+                commentDbContext = null;
+                SystemMessage = "PostgreSQL データベースへの接続に失敗しました";
+            }
         }
 
         public string Title
@@ -18,5 +31,7 @@
         }
 
         public string InputText { get => inputText; set => SetProperty(ref inputText, value); }
+
+        public string SystemMessage { get => systemMessage; set => SetProperty(ref systemMessage, value); }
     }
 }
