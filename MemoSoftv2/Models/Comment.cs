427cdfc322582287aef933e1146efcf34d69fad2
Akn <sibata486@gmail.com>
2022-08-30 19:49:36 +0900

    add / Comment クラスを追加

---
+++ MemoSoftv2/Models/Comment.cs
@@ -0,0 +1,24 @@
+﻿namespace MemoSoftv2.Models
+{
+    using System;
+
+    public class Comment
+    {
+        public Comment()
+        {
+            CreationDateTime = DateTime.Now;
+        }
+
+        public Comment(string text, DateTime creationDatetime)
+        {
+            Text = text;
+            CreationDateTime = creationDatetime;
+        }
+
+        public int Id { get; set; }
+
+        public string Text { get; set; } = string.Empty;
+
+        public DateTime CreationDateTime { get; set; }
+    }
+}
