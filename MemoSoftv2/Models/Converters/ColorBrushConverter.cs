b0e4964008492be00ab4a219b7050f1ed5878755
Akn <sibata486@gmail.com>
2022-09-02 18:03:05 +0900

    add / コメントの背景色の変更をビューに反映

---
+++ MemoSoftv2/Models/Converters/ColorBrushConverter.cs
@@ -0,0 +1,26 @@
+﻿namespace MemoSoftv2.Models.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class ColorBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var mc = default(System.Windows.Media.Color);
+            var dc = System.Drawing.Color.FromArgb((int)value);
+            mc.A = dc.A;
+            mc.R = dc.R;
+            mc.G = dc.G;
+            mc.B = dc.B;
+
+            return new System.Windows.Media.SolidColorBrush(mc);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
