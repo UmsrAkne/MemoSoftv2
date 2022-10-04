932d51b04040af795a92c5349d622ec3b4900693
Akn <sibata486@gmail.com>
2022-10-04 22:11:55 +0900

    refactor / SubComment の IDE の警告を抑制

--- MemoSoftv2/Models/SubComment.cs
+++ MemoSoftv2/Models/SubComment.cs
@@ -2,6 +2,7 @@ namespace MemoSoftv2.Models
 {
     public class SubComment : Comment
     {
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
         public int ParentCommentId { get; set; }
     }
 }
