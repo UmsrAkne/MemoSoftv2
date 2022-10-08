ae8ea3e699c65b499c6edb060f437e71269fedf5
Akn <sibata486@gmail.com>
2022-10-08 23:33:23 +0900

    add / 認証画面のコントロールとビューモデルのプロパティをバインド

--- MemoSoftv2/ViewModels/ConnectionPageViewModel.cs
+++ MemoSoftv2/ViewModels/ConnectionPageViewModel.cs
@@ -1,6 +1,23 @@
+using Prism.Mvvm;
+
 namespace MemoSoftv2.ViewModels
 {
-    public class ConnectionPageViewModel
+    public class ConnectionPageViewModel : BindableBase
     {
+        private int port;
+        private string databaseName;
+        private string host;
+        private string password;
+        private string userName;
+
+        public int Port { get => port; set => SetProperty(ref port, value); }
+
+        public string DatabaseName { get => databaseName; set => SetProperty(ref databaseName, value); }
+
+        public string Host { get => host; set => SetProperty(ref host, value); }
+
+        public string Password { get => password; set => SetProperty(ref password, value); }
+
+        public string UserName { get => userName; set => SetProperty(ref userName, value); }
     }
 }
